6978f7db42bab8e8cba912b0567488f216d132c5
Tntshafter <tntshafter@gmail.com>
2016-06-02 20:50:58 +0300

    Question.cpp
    not fully ready yet

---
+++ Question.cpp
@@ -0,0 +1,23 @@
+//file not fully ready yet
+#include "Question.h"
+
+Question::Question(int, string, string, string, string, string)
+{
+
+}
+string Question::getQuestion()
+{
+	return _question;
+}
+string* Question::getAnswers()
+{
+
+}
+int Question::getCorrectAnswerIndex()
+{
+
+}
+int Question::getId()
+{
+	return _id;
+}
