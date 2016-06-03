f541c6e2a867256d6ce150c9cbd4ed9ffd76a118
Tntshafter <tntshafter@gmail.com>
2016-06-03 13:01:36 +0300

    Helper problem
    the original helpers get runtime errors, not sure why. currently
    compiling is better without them

--- Helper.h
+++ Helper.h
@@ -13,14 +13,14 @@ class Helper
 {
 public:
 
-
-
-	static int getMessageTypeCode(SOCKET sc);
-	static char* getPartFromSocket(SOCKET sc, int bytesNum, int flags);
-	static int getIntPartFromSocket(SOCKET sc, int bytesNum);
-	static string getStringPartFromSocket(SOCKET sc, int bytesNum);
-	static void sendData(SOCKET sc, std::string message);
-	static string getPaddedNumber(int num, int digits);
+	 
+	 
+	 static int getMessageTypeCode(SOCKET sc);
+	 static char* getPartFromSocket(SOCKET sc, int bytesNum, int flags);
+	 static int getIntPartFromSocket(SOCKET sc, int bytesNum);
+	 static string getStringPartFromSocket(SOCKET sc, int bytesNum);
+	 static void sendData(SOCKET sc, std::string message);
+	 static string getPaddedNumber(int num, int digits);
 
 private:
 	static char* Helper::getPartFromSocket(SOCKET sc, int bytesNum);
