835f8717a3b290c5d1fb0516afbdaf09757849e4
Tntshafter <tntshafter@gmail.com>
2016-06-03 03:29:05 +0300

    Helper added
    ^

--- Validator.cpp
+++ Validator.cpp
@@ -1,10 +1,22 @@
 #include "Validator.h"
-
+#include "Helper.h"
+bool isLetter(char word)
+{
+	if ((word > 64 && word < 91) || (word > 96 && word < 123))
+	{
+		return true;
+	}
+	return false;
+}
 static bool isPasswordValid(string password)
 {
 
 }
 static bool isUsernameValid(string username)
 {
-
+	if (username.find(" ") || username.compare("") == 0 || !isLetter(username[0]))
+	{
+		return false;
+	}
+	return true;
 }
--- Validator.h
+++ Validator.h
@@ -3,3 +3,5 @@
 using namespace std;
 static bool isPasswordValid(string password);
 static bool isUsernameValid(string username);
+//functions added separately:
+ bool isLetter(char word); //checks if a char is a letter
