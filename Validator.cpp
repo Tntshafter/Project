bf622556975a7ad96b90de557256b2badab6ae18
Tntshafter <tntshafter@gmail.com>
2016-06-03 12:56:24 +0300

    Validator finished
    ^

--- Validator.cpp
+++ Validator.cpp
@@ -2,7 +2,7 @@
 #include "Helper.h"
 bool isLetter(char word)
 {
-	if ((word > 64 && word < 91) || (word > 96 && word < 123))
+	if ((word > CAP_LETTER_START && word < CAP_LETTER_CAP) || (word > SMALL_LETTER_START && word < SMALL_LETTER_CAP))
 	{
 		return true;
 	}
@@ -10,7 +10,40 @@ bool isLetter(char word)
 }
 static bool isPasswordValid(string password)
 {
-
+	bool lenFlag = false,
+		numFlag = false,
+		letterFlag = false,
+		capLetterFlag = false,
+		noSpaceFlag = true;
+	if (password.length > 3)
+	{
+		lenFlag = true;
+	}
+	if (password.find(" "))
+	{
+		noSpaceFlag = false;
+	}
+	int i;
+	for (i = 0; i < password.length; i++)
+	{
+		if (password[i] > SMALL_LETTER_START  && password[i] < SMALL_LETTER_CAP)
+		{
+			letterFlag = true;
+		}
+		if (password[i] > CAP_LETTER_START && password[i] < CAP_LETTER_CAP)
+		{
+			capLetterFlag = true;
+		}
+		if (password[i] > NUMBER_START && password[i] < NUMBER_END)
+		{
+			numFlag = true;
+		}
+	}
+	if (noSpaceFlag && numFlag && letterFlag && capLetterFlag && lenFlag)
+	{
+		return true;
+	}
+	return false;
 }
 static bool isUsernameValid(string username)
 {
--- Validator.h
+++ Validator.h
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <string>
+#define SMALL_LETTER_CAP 123
+#define SMALL_LETTER_START 96
+#define CAP_LETTER_CAP 91
+#define CAP_LETTER_START 64
+#define NUMBER_START 47
+#define NUMBER_END 58
 using namespace std;
 static bool isPasswordValid(string password);
 static bool isUsernameValid(string username);
