cb0b4e1dfb0da9fee232e3014c3c709adb8d2af4
Tntshafter <tntshafter@gmail.com>
2016-07-13 06:36:34 +0300

    more work
    this time triviaSerer, a quarter done.

--- TriviaServer.cpp
+++ TriviaServer.cpp
@@ -1,9 +1,35 @@
 #include "TriviaServer.h"
 #include "Protocol.h"
 #include "Helper.h"
+#include "Validator.h"
+typedef std::map<SOCKET,User * >::iterator typo; //to scan users
+Helper help;
 TriviaServer::TriviaServer()
 {
+	//database
 
+	//socket
+	WSADATA wsa;
+	SOCKET s;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		exit(1);
+	}
+
+	printf("Initialised.\n");
+
+
+	if ((s = socket(AF_INET, SOCK_STREAM, 8820)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+
+	
 }
 TriviaServer::~TriviaServer()
 {
@@ -27,9 +53,14 @@ void TriviaServer::clientHandler(SOCKET)
 {
 
 }
-void TriviaServer::safeDeleteUser(RecievedMessage*)
+void TriviaServer::safeDeleteUser(RecievedMessage*msg)
 {
-
+	handleSignout(msg);
+	int res = closesocket(msg->getSock());
+	if (res == SOCKET_ERROR)
+	{
+		cout << "An error occurred while disconnecting a client.\n";
+	}
 }
 
 User* TriviaServer::handleSignin(RecievedMessage* msg)
@@ -66,9 +97,40 @@ User* TriviaServer::handleSignin(RecievedMessage* msg)
 		return nullptr;
 	}
 }
-bool TriviaServer::handleSignup(RecievedMessage*)
+bool TriviaServer::handleSignup(RecievedMessage* msg)
 {
-
+	string username, pass, email;
+	//part one:
+	vector<string> vec = msg->getValues();
+	username = vec[0];
+	pass = vec[1];
+	email = vec[2];
+	//part two:
+	if (!isPasswordValid(pass))
+	{
+		help.sendData(msg->getSock(), ILLEGAL_PASSWORD);
+		return false;
+	}
+	if (!isUsernameValid(username))
+	{
+		help.sendData(msg->getSock(), ILLEGAL_USERNAME);
+		return false;
+	}
+	//part three:
+	if (_db.isUserExists(username))
+	{
+		help.sendData(msg->getSock(), USERNAME_EXISTS);
+		return false;
+	}
+	//part four:
+	if (!_db.addNewUser(username, pass, email))
+	{
+		help.sendData(msg->getSock(), UNKNOWN_SIGNUP_ERROR);
+		return false;
+	}
+	//part five:
+	help.sendData(msg->getSock(), SUCCESS_SIGNUP);
+	return true;
 }
 void TriviaServer::handleSignout(RecievedMessage* msg)
 {
@@ -90,14 +152,25 @@ void TriviaServer::handleStartGame(RecievedMessage*)
 {
 
 }
-void TriviaServer::handlePlayerAnswer(RecievedMessage*)
+void TriviaServer::handlePlayerAnswer(RecievedMessage* msg)
 {
-
+	User * user = getUserBySocket(msg->getSock());
+	bool res = user->getGame()->handleAnswerFromUser(user, msg->getValues[0], msg->getValues[1]);
+	if (res == false)
+	{
+		delete(user->getGame());
+		user->setGame(nullptr);
+	}
 }
 
-bool TriviaServer::handleCreateRoom(RecievedMessage*)
+bool TriviaServer::handleCreateRoom(RecievedMessage* msg)
 {
-
+	User * user = getUserBySocket(msg->getSock());
+	if (user == nullptr) return false;
+	_roomIdsequence++;
+	vector<string> values = msg->getValues();
+	bool flag =user->createRoom(_roomIdsequence, values[0], stoi(values[1]), stoi(values[2]), stoi(values[3]));
+	if (flag) _roomsList.insert(std::pair<int, Room *>(_roomIdsequence, user->getRoom()));
 }
 bool TriviaServer::handleCloseRoom(RecievedMessage*)
 {
@@ -142,16 +215,20 @@ RecievedMessage* TriviaServer::buildRecieveMessage(SOCKET, int)
 
 }
 
-User* TriviaServer::getUserByName(string)
+User* TriviaServer::getUserByName(string username)
 {
-
+	for (typo iterator = _connectedUsers.begin(); iterator != _connectedUsers.end(); iterator++) //casual scan through the map
+	{
+		if (iterator->second->getUsername().compare(username) == 0) return iterator->second;
+	}
+	return nullptr;
 }
-User* TriviaServer::getUserBySocket(SOCKET)
+User* TriviaServer::getUserBySocket(SOCKET sock)
 {
-
+	return _connectedUsers[sock];
 }
-Room* TriviaServer::getRoomById(int)
+Room* TriviaServer::getRoomById(int id)
 {
-
+	return _roomsList[id];
 }
 
